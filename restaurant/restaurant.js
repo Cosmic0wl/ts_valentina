aae7f63b124a41557181d2b94a4624ac1bbb2257
Cosmic0wl <valentinaspanetta@gmail.com>
2019-06-12 21:35:44 +0200

    items from menu

--- restaurant/restaurant.js
+++ restaurant/restaurant.js
@@ -1,6 +1,47 @@
-var menu = ['pasta', 'salmon', 'toast'];
-var prices = ['14$', '19$', '8$'];
-var first = menu[0], second = menu[1], third = menu[2], fourth = menu[3];
-var one = prices[0], two = prices[1], three = prices[2], four = prices[3];
-for (var i = 0; i < menu.length; i++) {
+var menu = [{
+        name: "Pasta",
+        price: 14,
+        rating: 0
+    }, {
+        name: "Salmon",
+        price: 19,
+        rating: 0
+    }, {
+        name: "Popsicle",
+        price: 8,
+        rating: 0
+    },
+];
+menu.forEach(function (value) {
+    var name = value.name, price = value.price;
+    document.getElementsByClassName("card-title")[menu.indexOf(value)].innerHTML = name;
+    document.getElementsByClassName("card-text")[menu.indexOf(value)].innerHTML = price + "$";
+});
+// var btn = document.getElementsByClassName("btn");
+// var i = 0;
+// btn.forEach(function(value){
+// 	value.addEventListener("click", function(), starRatingByColor);
+// 	i++;
+// });
+// function starRatingByColor() {
+// 	var star = document.getElementsByClassName("fa");
+// 	star.classList.add("checked");
+// }
+var stars = document.getElementsByClassName("fa");
+var i = 0;
+menu.forEach(function (menuItem) {
+    var j = 0;
+    var _loop_1 = function (j_1) {
+        var star = stars[i * 5 + j_1];
+        console.log(star);
+        star.addEventListener("click", function () { setRating(menuItem, j_1 + 1); });
+    };
+    for (var j_1 = 0; j_1 < 5; j_1++) {
+        _loop_1(j_1);
+    }
+    i++;
+});
+function setRating(item, rating) {
+    item.rating = rating;
+    console.log(item);
 }
--- restaurant/restaurant.ts
+++ restaurant/restaurant.ts
@@ -1,14 +1,49 @@
+const menu : Array<any> = [{
+	name: "Pasta",
+	price: 14,
+	rating: 0
+}, {
+	name: "Salmon",
+	price: 19,
+	rating: 0
+},{
+	name: "Popsicle",
+	price: 8,
+	rating: 0
+},
+];
 
-const menu : Array<string> = ['pasta','salmon','toast'];
-const prices : Array<string> = ['14$', '19$', '8$'];
+menu.forEach(function(value){
+	let {name, price} = value;
+	document.getElementsByClassName("card-title")[menu.indexOf(value)].innerHTML = name;
+	document.getElementsByClassName("card-text")[menu.indexOf(value)].innerHTML = price + "$";
+});
 
-const [first, second, third, fourth] = menu;
-const [one, two, three, four] = prices;
+// var btn = document.getElementsByClassName("btn");
+// var i = 0;
+// btn.forEach(function(value){
+// 	value.addEventListener("click", function(), starRatingByColor);
+// 	i++;
+// });
 
+// function starRatingByColor() {
+// 	var star = document.getElementsByClassName("fa");
+// 	star.classList.add("checked");
+// }
 
+var stars = document.getElementsByClassName("fa");
+var i:number = 0;
+menu.forEach(function(menuItem){
+	let j:number = 0;
+	for (let j = 0; j < 5; j++) {
+		let star = stars[i * 5 + j];
+		console.log(star);
+		star.addEventListener("click", function() {setRating(menuItem,j+1)});
+	}
+	i++;
+});
 
-for(let i = 0 ; i < menu.length ; i++){
-
-	
-
+function setRating(item : any, rating : number){
+	item.rating = rating;
+	console.log(item);
 }
